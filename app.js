85a3f91c498c1ec740a7ea0ed8231f01100eb611
TamaWira <asusrog20012001@gmail.com>
2023-10-04 22:44:02 +0800

    Update: Update feature nominal rupiah

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const DISCOUNT = 0.1; // 10%
-const SHIPPING_FEE = 100;
+const SHIPPING_FEE = 1000;
 
 const productText = document.getElementById("product-text");
 const subTotalText = document.getElementById("subtotal-text");
@@ -7,8 +7,8 @@ const discountText = document.getElementById("discount-text");
 const shippingFeeText = document.getElementById("shipping-fee-text");
 const totalText = document.getElementById("total-text");
 
-discountText.innerText = DISCOUNT;
-shippingFeeText.innerText = SHIPPING_FEE;
+discountText.innerText = DISCOUNT*100 + "%";
+shippingFeeText.innerText = SHIPPING_FEE ;
 
 const inputItemName = document.getElementById("itemName");
 const inputPrice = document.getElementById("price");
@@ -23,17 +23,20 @@ function onSubmitCart(e) {
   e.preventDefault();
 
   if (
-    inputPrice.value < 0 ||
-    inputQty.value < 0 
+    inputPrice.value < 0 || inputPrice.value === "" ||
+    inputQty.value < 0 || inputQty.value === ""
   ) {
-    totalText.innerText = "Nilai tidak boleh negatif atau tidak angka";
+    totalText.innerText = "Nilai tidak boleh negatif atau kosong";
   } else {
-    const subTotal = inputPrice.value * inputQty.value;
-    subTotalText.innerText = subTotal;
-
-    const Total = subTotal * (1 - DISCOUNT) + SHIPPING_FEE;
-    totalText.innerText = Total;
+    const subTotal = inputPrice.value * inputQty.value
+    const Total = (subTotal * (1 - DISCOUNT) + SHIPPING_FEE).toLocaleString('id-ID');
+    const discountNominal = (subTotal * DISCOUNT).toLocaleString('id-ID');
+    
+    subTotalText.innerText = `Rp. ${subTotal.toLocaleString('id-ID')}`;
+    discountText.innerText = `Rp. ${discountNominal}`;
+    totalText.innerText = `Rp. ${Total}`;
   }
+
 }
 
 inputItemName.addEventListener("change", onChangeInputName);
