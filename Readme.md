a1a1437a9dba0bab1b1e52dc0eba97af035620a7
TamaWira <asusrog20012001@gmail.com>
2023-10-04 21:36:58 +0800

    init: Create project kelompok 4 fix all features

---
+++ Readme.md
@@ -0,0 +1,5 @@
+Kelompok 1:
+- I Putu Bayu Adhya Wiratama
+- Faridz Nur Firdausy Setyo Nugraha
+- David Nasrulloh
+- Sahat Parulian Panggabean
--- app.js
+++ app.js
@@ -1,22 +1,40 @@
 const DISCOUNT = 0.1; // 10%
 const SHIPPING_FEE = 100;
 
-const productText = document.getElementById('product-text');
-const subTotalText = document.getElementById('subtotal-text');
-const discountText = document.getElementById('discount-text');
-const shippingFeeText = document.getElementById('shipping-fee-text');
-const totalText = document.getElementById('total-text');
+const productText = document.getElementById("product-text");
+const subTotalText = document.getElementById("subtotal-text");
+const discountText = document.getElementById("discount-text");
+const shippingFeeText = document.getElementById("shipping-fee-text");
+const totalText = document.getElementById("total-text");
 
-discountText.innerHTML = DISCOUNT;
-shippingFeeText.innerHTML = SHIPPING_FEE;
+discountText.innerText = DISCOUNT;
+shippingFeeText.innerText = SHIPPING_FEE;
 
-const inputItemName = document.getElementById('itemName');
-const inputPrice = document.getElementById('price');
-const inputQty = document.getElementById('qty');
-const formCart = document.getElementById('form-cart');
+const inputItemName = document.getElementById("itemName");
+const inputPrice = document.getElementById("price");
+const inputQty = document.getElementById("qty");
+const formCart = document.getElementById("form-cart");
 
-function onChangeInputName(e) {}
-function onSubmitCart(e) {}
+function onChangeInputName() {
+    productText.innerText = inputItemName.value;
+}
 
-inputItemName.addEventListener('change', onChangeInputName);
-formCart.addEventListener('submit', onSubmitCart);
+function onSubmitCart(e) {
+  e.preventDefault();
+
+  if (
+    inputPrice.value < 0 ||
+    inputQty.value < 0 
+  ) {
+    totalText.innerText = "Nilai tidak boleh negatif atau tidak angka";
+  } else {
+    const subTotal = inputPrice.value * inputQty.value;
+    subTotalText.innerText = subTotal;
+
+    const Total = subTotal * (1 - DISCOUNT) + SHIPPING_FEE;
+    totalText.innerText = Total;
+  }
+}
+
+inputItemName.addEventListener("change", onChangeInputName);
+formCart.addEventListener("submit", onSubmitCart);
